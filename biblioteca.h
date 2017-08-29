b4266afff39cb1b04dd69ef68568ccf0aa804c35
Franco Zarate <fran.e.zarate@gmail.com>
2017-08-29 10:34:20 -0300

    Create biblioteca.h

---
+++ biblioteca.h
@@ -0,0 +1,26 @@
+/** \brief Realiza la suma entre dos numeros
+ *
+ * \param Valor de tipo float que representa un numero
+ * \param Valor de tipo float que representa un numero
+ * \return Valor de tipo float que representa el resultado de la suma entre dos numeros
+ *
+ */
+float sumar(float,float);
+
+/** \brief Realiza la resta entre dos numeros
+ *
+ * \param Valor de tipo float que representa un numero
+ * \param Valor de tipo float que representa un numero
+ * \return  Valor de tipo float que representa el resultado de la resta entre dos numeros
+ *
+ */
+ float restar (float,float);
+
+ /** \brief Realiza la multiplicacion entre dos numeros
+  *
+  * \param Valor de tipo float que representa un numero
+  * \param Valor de tipo float que representa un numero
+  * \return  Valor de tipo float que representa el resultado de la multiplicacion entre dos numeros
+  *
+  */
+  float multiplicar (float,float);
