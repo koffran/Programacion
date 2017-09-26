56cd2294dd3b3c932df7d2d81d58bdc2ed127775
Franco Zarate <fran.e.zarate@gmail.com>
2017-09-26 12:06:26 -0300

    Update main.c

--- Spotify/main.c
+++ Spotify/main.c
@@ -1,106 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "funciones.h"
 #define TC 7
 #define TI 5
-
-typedef struct
-{
-    int idCancion;
-    int interprete;
-    char titulo [50];
-    int duracion;
-}eCancion;
-
-typedef struct
-{
-    int idInterprete;
-    char nombre [50];
-    char nacionalidad [50];
-}eInterprete;
-
-void inicializarCancion (eCancion lista[],int tam);
-void inicializarInterprete (eInterprete lista[],int tam);
-
-void listarCanciones (eCancion lista[],int tam);
-void listarInterpretes (eInterprete lista[],int tam);
 
 int main()
 {
     eCancion listaCanciones [TC];
     eInterprete listaInterpretes [TI];
-
-    inicializarCancion(listaCanciones,TC);
-    inicializarInterprete(listaInterpretes,TI);
-    int i;
-    eCancion aux[][50];
+    int i, opcion;
+    eCancion aux[TC][50];
 
     for (i=0;i<TC;i++)
     {
-        if (listaCanciones[i].interprete == listaInterpretes[i].idInterprete)
-        {
-            strcpy
-        }
+        listaCanciones[i].estado = 0;
+    }
+     for (i=0;i<TI;i++)
+    {
+        listaInterpretes[i].estado = 0;
     }
 
+    do
+    {
 
-    listarCanciones (listaCanciones,TC);
-    listarInterpretes (listaInterpretes,TI);
+        printf("1 - Inicializar canciones\n2 - Inicializar Interpretes\n3 - Listar Canciones\n4 - Listar Interpretes\n5 - Listar Canciones con interpretes\n6 - Listar canciones por interprete\n7 - Listar canciones estadounidenses\n8 - SALIR\n");
+        scanf("%d", &opcion);
+
+        switch (opcion)
+        {
+        case 1:
+            inicializarCancion(listaCanciones,TC);
+            break;
+        case 2:
+            inicializarInterprete(listaInterpretes,TI);
+            break;
+        case 3:
+            listarCanciones (listaCanciones,TC);
+            break;
+        case 4:
+            listarInterpretes (listaInterpretes,TI);
+            break;
+        case 5:
+            listarCancionesConInterprete (listaCanciones, TC,listaInterpretes,TI);
+            break;
+        case 6:
+            cancionesPorInterprete (listaCanciones,TC,listaInterpretes,TI);
+            break;
+        case 7:
+            listarCancionesEstadounidenses (listaCanciones,TC,listaInterpretes,TI);
+            break;
+        case 8:
+            break;
+        default:
+            printf("Ingrese una opcion valida\n");
+            //scanf("%d", &opcion);
+            break;
+        }
+
+    }while (opcion != 8);
 
     return 0;
-}
-
-void inicializarCancion (eCancion lista[],int tam)
-{
-    int i;
-    int id [] = {1,2,3,4,5,6,7};
-    char titulo [][30] = {"Escape","Hotel California","Bohemian Rhapsody","Cocaine","Smooth criminal","Of wolf and man","Brighton rock"};
-    int interprete [] = {10,11,12,13,14,10,12};
-    int duracion []= {3,4,7,4,9,5,4};
-
-    for (i=0;i<tam;i++)
-    {
-        lista[i].idCancion = id[i];
-        strcpy (lista[i].titulo, titulo[i]);
-        lista[i].duracion = duracion [i];
-        lista[i].interprete = interprete[i];
-    }
-
-}
-
-void inicializarInterprete (eInterprete lista[],int tam)
-{
-    int i;
-    int id [] = {10,11,12,13,14};
-    char nombre [][50] = {"Metallica","The eagles","Queen","Eric Clapton","Michael Jackson"};
-    char nacionalidad [][30] = {"Estadounidense","Estadounidense","Britanica","Britanica","Estadounidense"};
-
-    for (i=0;i<tam;i++)
-    {
-        lista[i].idInterprete = id[i];
-        strcpy (lista[i].nombre, nombre[i]);
-        strcpy (lista[i].nacionalidad, nacionalidad[i]);
-    }
-
-}
-
-void listarCanciones (eCancion lista[],int tam)
-{
-    int i;
-    printf("Canciones:\n");
-
-    for (i=0;i<tam;i++)
-    {
-        printf("%d---%s---%d minutos---%d interprete\n",lista[i].idCancion,lista[i].titulo,lista[i].duracion,lista[i].interprete);
-    }
-}
-
-void listarInterpretes (eInterprete lista[],int tam)
-{
-    int i;
-    printf("Interpretes:\n");
-
-    for (i=0;i<tam;i++)
-    {
-        printf("%d---%s---%s\n",lista[i].idInterprete,lista[i].nombre,lista[i].nacionalidad);
-    }
 }
